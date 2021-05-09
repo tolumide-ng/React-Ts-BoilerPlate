6db3d5b15a697210d75b4999a923190cc5a50781
tolumide-ng <tolumideshopein@gmail.com>
2021-05-09 12:28:34 +0100

    removes unused libraries

--- src/main/components/Pages/LandingPage/index.tsx
+++ src/main/components/Pages/LandingPage/index.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
-import { LandingRight } from "../../UI/organims/LandingRight";
 import "./index.css";
 
 export const LandingPage = () => {
     return (
         <article className="ldpg">
-            <article className="ldpg-cont">
-                <article className="ldpg-left"></article>
-                <article className="ldpg-right">
-                    <LandingRight />
-                </article>
-            </article>
+            <article className="ldpg-cont"></article>
         </article>
     );
 };
