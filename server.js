4e0d324086393417b29769d824d6c06b9cf4a868
tolumide-ng <tolumideshopein@gmail.com>
2021-05-09 12:24:46 +0100

    removes unused libaries and bumps up axios

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const compression = require("compression");
 require("dotenv").config();
 const path = require("path");
 
 const port = process.env.PORT || 9000;
 const app = express();
-
-app.use(compression);
 
 app.use(express.static(`${__dirname}/dist`));
 
