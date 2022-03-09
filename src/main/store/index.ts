bfa055b97c4f30d7eca28dde2b90c2e1179952c0
tolumide-ng <tolumideshopein@gmail.com>
2022-03-09 23:47:11 +0100

    Updates some libraries

--- src/main/store/index.ts
+++ src/main/store/index.ts
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { rootReducer } from "./modules";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const storeEnhancer = composeWithDevTools || compose;
 const middlewares = [thunkMiddleware];
 
 export const store = createStore(
-    rootReducer,
-    storeEnhancer(applyMiddleware(...middlewares))
+  rootReducer,
+  storeEnhancer(applyMiddleware(...middlewares))
 );
 
 export default store;
